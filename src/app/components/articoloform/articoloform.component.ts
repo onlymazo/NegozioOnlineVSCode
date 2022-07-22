55fab7662e1468a472c7c0b0ca329943e9c7be35
psicosato <salvatore.carlucci@gmail.com>
2022-07-22 20:39:27 +0200

    dsadsa

--- src/app/components/articoloform/articoloform.component.ts
+++ src/app/components/articoloform/articoloform.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Articolo } from 'src/app/articolo';
+import { ArticoloService } from 'src/app/services/articolo.service';
 
 @Component({
   selector: 'app-articoloform',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articoloform.component.css']
 })
 export class ArticoloformComponent implements OnInit {
-
-  constructor() { }
+  articolo!: Articolo;
+  constructor(private _articoloService: ArticoloService, private _router: Router) { }
 
   ngOnInit(): void {
+    this.articolo = this._articoloService.getter();
   }
 
+  processaForm() {
+    if (this.articolo.id == undefined) {
+
+      this._articoloService.createArticolo(this.articolo).subscribe((a) => {
+        console.log(a);
+        this._router.navigate(['/']);
+      });
+    } else {
+      this._articoloService.updateArticolo(this.articolo).subscribe((a) => {
+        console.log(a);
+        this._router.navigate(['/']);
+      });
+    }
+  }
 }
